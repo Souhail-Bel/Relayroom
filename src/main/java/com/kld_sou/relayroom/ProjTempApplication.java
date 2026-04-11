f3e8c587264abd5404bb52c94379eb039166a68f
Souhail-Bel <robotboy976@gmail.com>
2026-04-11 19:33:17 +0100

    Project initialization

---
+++ src/main/java/com/kld_sou/relayroom/ProjTempApplication.java
@@ -0,0 +1,12 @@
+package com.kld_sou.relayroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjTempApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjTempApplication.class, args);
+  }
+}
---
+++ src/test/java/com/kld_sou/relayroom/ProjTempApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kld_sou.relayroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjTempApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
